c9cf20492c03901339a16d8e849cd3f2239ad65c
Vindu97 <vindulalakshani1997@gmail.com>
2023-02-05 13:00:24 +0530

    first commit

---
+++ src/main/java/com/example/demo/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class BaseController {
+
+    @GetMapping("/")
+    public String indexPage() {
+        log.debug("Index Page Opened");
+        return "index";
+    }
+}
